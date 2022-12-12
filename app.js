ba43ebad2abb160779bfe1aec3b9167fcf27eab6
Silas Martins <silas.martins2041@gmail.com>
2022-12-12 15:28:23 -0300

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var valorEmDolarTexto = prompt("Qual o valor em dolar que você quer converter?")
+
+var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+var valorEmReal = valorEmDolarNumero * 5.32
+var valorEmRealFixado = valorEmReal.toFixed(2)
+
+alert(valorEmRealFixado)
